32c1f2c3b8bc2db83d5f48780c24f6fa4120ed76
sineunju <eunjuoi0515@gmail.com>
2019-07-15 18:09:54 +0900

    [MODIFIED] 함수 및 class 네이밍, vault_client token 추가 부분 변경
    적절하지 않는 함수와 class 네이밍을 변경하였고 vault client에 연결하는 token 코드 변경했습니다

--- ysl/config/vault.py
+++ ysl/config/vault.py
@@ -9,33 +9,25 @@ VAULT_DB_URL = 'database/creds/yves-saint-laurent-{env}'
 def create_vault_client():
     client = hvac.Client(url=VAULT_URL)
 
-    if os.environ.get("VAULT_TOKEN"):
-        client.token = os.environ["VAULT_TOKEN"]
-    elif os.environ.get("GITHUB_TOKEN"):
-        client.auth.github.login(token=os.environ.get("GITHUB_TOKEN"))
+    client.auth.github.login(token=os.environ.get("GITHUB_TOKEN"))
 
     return client
 
 
-def db_credential_url(env):
+def get_db_credential_url(env):
     env = 'prod' if env == 'production' else 'test'
     return VAULT_DB_URL.format(env=env)
 
 
-def vault_secret_url(env):
-    env = 'prod' if env == 'production' else 'test'
-    return VAULT_SECRET_CONFIG_URL.format(env=env)
-
-
-def config(env):
+def get_config(env):
     client = create_vault_client()
 
-    database_credential = client.read(db_credential_url(env=env))['data']
+    database_credential = client.read(get_db_credential_url(env=env))['data']
 
-    get_config = {
+    config = {
         'env': env,
         'DATABASE_USERNAME': database_credential.values(),
         'DATABASE_PASSWORD': database_credential.values()
     }
 
-    return get_config
+    return config
