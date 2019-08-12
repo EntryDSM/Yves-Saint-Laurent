44e761d4b9d2733323c3df79b6031cc88d0c762f
sineunju <eunjuoi0515@gmail.com>
2019-08-12 17:48:15 +0900

    [ADD] admin관련 api
    면접관 수락, 거절, 기관 정보보기, 기관삭제, 신청한 면접관 리스트 보기

---
+++ ysl/api/admin.py
@@ -0,0 +1,50 @@
+from flask import Blueprint, request, Response
+from flask_restful import Resource, Api
+
+from ysl.db.agency import Agency
+from ysl.db.apply_interviewer import ApplyInterviewer
+from ysl.db import db
+
+bp_admin = Blueprint("admin", __name__, url_prefix="/api/admin")
+api_admin = Api(bp_admin)
+
+
+class Agency(Resource):
+    def get(self):
+        agency_code = request.view_args['agency_code']
+
+        agency = Agency.query.filter_by(code=agency_code).first()
+
+        return Response({
+            "agency_name": agency.name,
+            "agency_kind": agency.kind,
+            "agency_explanation": agency.explanation,
+            "agency_code": agency.code
+        }, 200)
+
+    def delete(self):
+        agency_code = request.view_args['agency_code']
+
+        agency = Agency.query.filter_by(code=agency_code).first()
+
+        db.session.delete(agency)
+        db.session.commit()
+
+        return Response({"msg": "successful agency delete"}, 200)
+
+
+class Interviewer(Resource):
+    def get(self):
+        agency_code = request.view_args['agency_code']
+
+        interviewer_list = ApplyInterviewer.query.filter(ApplyInterviewer.agency == agency_code)
+
+        return Response({"interviewer": [
+            {
+                "interviewer_name": interviewer.name
+            } for interviewer in interviewer_list]
+        })
+
+
+api_admin.add_resource(Agency, "/<agency_code>")
+api_admin.add_resource(Interviewer, "/<agency_code>/interviewer")
---
+++ ysl/api/interviewer.py
@@ -0,0 +1,46 @@
+from flask import request, Response
+from flask_restful import Resource
+
+from ysl.db.belong import Belong
+from ysl.db.apply_interviewer import ApplyInterviewer
+from ysl.db import db
+from ysl.api.admin import api_admin
+
+
+class AcceptInterviewer(Resource):
+    def post(self):
+        agency_code = request.view_args['agency_code']
+        interviewer = request.json['user_email']
+
+        add_interviewer = Belong(agency=agency_code, interviewer=interviewer)
+        db.session.add(add_interviewer)
+
+        delete_interviewer = ApplyInterviewer.query.filter_by(
+            ApplyInterviewer.agency == agency_code
+            and ApplyInterviewer.interviewer == interviewer
+        ).first()
+        db.session.delete(delete_interviewer)
+
+        db.session.commit()
+
+        return Resource({"msg": "Successful interviewer accept"}, 200)
+
+
+class RejectInterviewer(Resource):
+    def post(self):
+        agency_code = request.view_args['agency_code']
+        interviewer = request.json['user_email']
+
+        delete_interviewer = ApplyInterviewer.query.filter_by(
+            ApplyInterviewer.agency == agency_code
+            and ApplyInterviewer.interviewer == interviewer
+        ).first()
+        db.session.delete(delete_interviewer)
+
+        db.session.commit()
+
+        return Resource({"msg": "Successful interviewer reject"}, 200)
+
+
+api_admin.add_resource(AcceptInterviewer, "/<agency_code>/interviewer/accept")
+api_admin.add_resource(RejectInterviewer, "/<agency_code>/interviewer/reject")
