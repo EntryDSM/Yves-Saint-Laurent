3ba10ad5f559899e80f00f0a0fce1ed5bfceb18c
sineunju <eunjuoi0515@gmail.com>
2019-09-04 23:32:04 +0900

    [ADD] json format check decorator
    json으로 왔는지 뿐만아니라 형식이 올바른지 검사하는 코드를 추가했다

--- ysl/api/__init__.py
+++ ysl/api/__init__.py
@@ -1,16 +1,22 @@
 from flask import request, abort
-from flask_jwt_extended  import get_jwt_identity
+from flask_jwt_extended import get_jwt_identity, jwt_required
 from functools import wraps
 
 from ysl.db.agency import Agency
+from ysl.db import session
 
 
-def check_json():
+def check_json(json):
     def decorator(func):
         @wraps(func)
         def wrapeer(*args, **kwargs):
             if not request.is_json:
                 abort(400, "Check json format")
+
+            for k, v in json.items():
+                if k not in request.json or type(request.json[k]) is not v:
+                    abort(400, "Check you json key name and value type")
+
             return func(*args, **kwargs)
         return wrapeer
     return decorator
@@ -19,9 +25,29 @@ def check_json():
 def check_admin():
     def decorator(func):
         @wraps(func)
+        @jwt_required
         def wrapeer(*args, **kwargs):
-            if not Agency.query.filter(email=get_jwt_identity()).first():
+
+            agency = session.query(Agency).query.filter(Agency.email == get_jwt_identity()).first()
+
+            if not agency:
                 abort(403, "No access rights")
+
             return func(*args, **kwargs)
         return wrapeer
+    return decorator
+
+
+def check_agency():
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+
+            agency = session.query(Agency).query.filter(Agency.code == get_jwt_identity()).first()
+
+            if not agency:
+                abort(400, "unseen agency")
+
+            return func(*args, **kwargs)
+        return wrapper
     return decorator
