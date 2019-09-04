2368f76aa37802fae3a0ae0c1f0cf5e4a026a4a4
sineunju <eunjuoi0515@gmail.com>
2019-09-04 23:38:19 +0900

    [ADD] agency code check api
    면접관이 가입할때 사용하는 agency code check api를 예외처리하고 추가했습니다.

--- ysl/api/interviewer.py
+++ ysl/api/interviewer.py
@@ -1,51 +1,24 @@
-from flask import request, Response
-from flask_restful import Resource
+from flask import Blueprint, request
+from flask_restful import Resource, Api
 
-from ysl.db.belong import Belong
-from ysl.db.apply_interviewer import ApplyInterviewer
-from ysl.db import db
-from ysl.api.admin import api_admin
-from ysl.api import check_admin, check_json
+from ysl.db import session
+from ysl.db.agency import Agency
 
-
-class AcceptInterviewer(Resource):
-    @check_admin()
-    @check_json()
-    def post(self):
-        agency_code = request.view_args['agency_code']
-        interviewer = request.json['user_email']
-
-        add_interviewer = Belong(agency=agency_code, interviewer=interviewer)
-        db.session.add(add_interviewer)
-
-        delete_interviewer = ApplyInterviewer.query.filter_by(
-            ApplyInterviewer.agency == agency_code
-            and ApplyInterviewer.interviewer == interviewer
-        ).first()
-        db.session.delete(delete_interviewer)
-
-        db.session.commit()
-
-        return Response({"msg": "Successful interviewer accept"}, 200)
+bp_agency = Blueprint("agency", __name__, url_prefix="/api/agency")
+api_agency = Api(bp_agency)
 
 
-class RejectInterviewer(Resource):
-    @check_admin()
-    @check_json()
-    def post(self):
-        agency_code = request.view_args['agency_code']
-        interviewer = request.json['user_email']
+class CheckAgencyCode(Resource):
+    def get(self):
+        agency_code = request.args.get('code')
 
-        delete_interviewer = ApplyInterviewer.query.filter_by(
-            ApplyInterviewer.agency == agency_code
-            and ApplyInterviewer.interviewer == interviewer
-        ).first()
-        db.session.delete(delete_interviewer)
+        agency = session.query(Agency).filter(Agency.code == agency_code).first()
 
-        db.session.commit()
-
-        return Response({"msg": "Successful interviewer reject"}, 200)
+        if agency:
+            return {"agency_name": agency.name,
+                    "agency_explanation": agency.explanation}, 200
+        else:
+            return {"msg": "unseen agency"}, 400
 
 
-api_admin.add_resource(AcceptInterviewer, "/<agency_code>/interviewer/accept")
-api_admin.add_resource(RejectInterviewer, "/<agency_code>/interviewer/reject")
+api_agency.add_resource(AgencyCode, "/check")
