40f15f3916cb28a63008e5d097b384618cfb155d
sineunju <eunjuoi0515@gmail.com>
2019-09-05 19:50:59 +0900

    [FIX] 환경변수로 db 주소 불러오기

--- ysl/config/config.py
+++ ysl/config/config.py
@@ -5,6 +5,8 @@ class Config:
     SYSTEM_NAME = 'Yves-Saint-Laurent'
 
 
+
+
 class TestConfig(Config):
     HOST = ''
     PORT = 5000
@@ -13,7 +15,9 @@ class TestConfig(Config):
 
     #DATABASE_URL = get_db_credential_url('test')
     #로컬테스트용 db url
-    DATABASE_URL = 'mysql://admin:20020515@ysl-test.cmdmfdz0ht7a.ap-northeast-2.rds.amazonaws.com:13306/test'
+    DATABASE_URL = os.getenv("DATABASE_URL")
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
     JWT_ACCESS_TOKEN_EXPIRES = os.getenv("JWT_ACCESS_TOKEN_EXPIRES")
     JWT_REFRESH_TOKEN_EXPIRES = os.getenv("JWT_REFRESH_TOKEN_EXPIRES")
+
+
