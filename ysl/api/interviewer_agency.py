800583c2906b1f265c46dec79ef8626f23f32b82
sineunju <eunjuoi0515@gmail.com>
2019-09-13 00:04:23 +0900

    [ADD] 기관 소속 신청하기 api 구현
    면접자가 기관에 소속 신청을 하는 api를 구현했습니다

--- ysl/api/interviewer_agency.py
+++ ysl/api/interviewer_agency.py
@@ -1,13 +1,15 @@
-from flask import abort
+from flask import abort, request
 from flask_restful import Resource
 from flask_jwt_extended import jwt_required, get_jwt_identity
 
 from ysl.db import session
 from ysl.db.belong import Belong
-from ysl.api.interviewer import api_agency
+from ysl.db.agency import Agency
+from ysl.db.apply_interviewer import ApplyInterviewer
+from ysl.api.interviewer import api_interviewer
 
 
-class JoinAgency(Resource):
+class JoinedAgencyList(Resource):
     @jwt_required
     def get(self):
         interviewer = get_jwt_identity()
@@ -24,4 +26,23 @@ class JoinAgency(Resource):
             abort("None Resources", 400)
 
 
-api_agency.add_resource(JoinAgency, "/agency")
+class ApplyToAgency(Resource):
+    @jwt_required
+    def post(self):
+        agency_code = request.json["agency_code"]
+        interviewer = get_jwt_identity()
+
+        agency = session.query(Agency).filter(Agency.code == agency_code).first()
+
+        if agency:
+            add_apply_interviewer = ApplyInterviewer(agency=agency_code, interviewer=interviewer)
+
+            session.add(add_apply_interviewer)
+            session.commit()
+            return {"msg": "Successful apply agency"}, 200
+        else:
+            abort(400, " It's agency that doesn't exist.")
+
+
+api_interviewer.add_resource(JoinedAgencyList, "/agency")
+api_interviewer.add_resource(ApplyToAgency, "/agency/join")
