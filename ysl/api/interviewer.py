0ddb7993cfe4a8a529323f5ed30dad0b31d5b5c4
sineunju <eunjuoi0515@gmail.com>
2019-09-12 23:24:25 +0900

    [ADD] interviewer 회원가입, 로그인 api 구현
    면접관이 회원가입, 로그인을 하는 api를 구현하였습니다.

--- ysl/api/interviewer.py
+++ ysl/api/interviewer.py
@@ -1,11 +1,14 @@
 from flask import Blueprint, request, abort
 from flask_restful import Resource, Api
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import create_access_token, create_refresh_token
 
 from ysl.db import session
+from ysl.db.interviewer import Interviewer
 from ysl.db.agency import Agency
 
-bp_agency = Blueprint("agency", __name__, url_prefix="/api/agency")
-api_agency = Api(bp_agency)
+bp_interviewer = Blueprint("agency", __name__, url_prefix="/api/v1")
+api_interviewer = Api(bp_interviewer)
 
 
 class CheckAgencyCode(Resource):
@@ -21,4 +24,42 @@ class CheckAgencyCode(Resource):
             return abort(400, "unseen agency")
 
 
-api_agency.add_resource(CheckAgencyCode, "/check")
+class InterviewerSignup(Resource):
+    def post(self):
+        name = request.json["name"]
+        email = request.json["email"]
+        password = generate_password_hash(request.json["password"])
+
+        interviewer = session.query(Interviewer).filter(Interviewer.email == email).first()
+
+        if interviewer:
+            abort(409, "This email has already been signed up")
+        else:
+            add_interviewer = Interviewer(name=name, email=email, pw=password)
+            session.add(add_interviewer)
+            session.commit()
+            return {"msg": "Successful signup to interviewer"}, 409
+
+
+class InterviewerLogin(Resource):
+    def post(self):
+        email = request.json["email"]
+        password = request.json["password"]
+
+        interviewer = session.query(Interviewer).filter(
+            Interviewer.email == email and check_password_hash(Interviewer.pw, password))
+
+        if interviewer:
+            return {
+                    "access": create_access_token(identity=email),
+                    "refresh": create_refresh_token(identity=email)
+            }, 200
+        else:
+            return abort(400, "Check email and password")
+
+
+api_interviewer.add_resource(CheckAgencyCode, "/agency/check")
+api_interviewer.add_resource(InterviewerSignup, "/interviewer/signup")
+api_interviewer.add_resource(InterviewerLogin, "/interviewer/login")
+
+
