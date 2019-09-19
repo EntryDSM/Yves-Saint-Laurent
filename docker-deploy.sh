2c5b36e881cb24d609507869fe790f9e4aa7988e
sineunju <eunjuoi0515@gmail.com>
2019-09-19 20:29:35 +0900

    [FIX] 오타

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -1,25 +1,26 @@
 #!/usr/bin/env bash
 
-version = "python -c import ysl; print(ysl.__version__)"
+version = `"python -c import ysl; print(ysl.__version__)"`
 
-echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" -- password-stdin registry.entrydsm.hs.kr"
+echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin registry.entrydsm.hs.kr
 
-if [[ "$1" == "api"]];then
+if [[ "$1" == "api" ]];then
     echo "Docker build on dev startred"
 
     docker build -t registry.entrydsm.hs.kr/ysl:dev .
 
     docker push registry.entrydsm.hs.kr/ysl:dev
+
 elif [["$1" == "master"]];then
-    echo "Docker buile on master started"
+    echo "Docker build on master started"
 
     docker build -t registry.entrydsm.hs.kr/ysl:${version} .
 
-    docker tag registry.entrydsm.hs.kr/ysl:${version} registry.entrydsm.hs.kr/hermes:latest
+    docker tag registry.entrydsm.hs.kr/ysl:${version} registry.entrydsm.hs.kr/ysl:latest
 
     docker push registry.entrydsm.hs.kr/ysl:${version}
     docker push registry.entrydsm.hs.kr/ysl:latest
 
 fi
 
-exit
+exit 0
