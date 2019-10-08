6dbdd26dbfe23474481cf455d910fa829557a833
sineunju <eunjuoi0515@gmail.com>
2019-10-08 10:47:25 +0900

    [ADD] 면접관 엑세스 확인하는 함수추가
    면접관이 이 면접에 할당되었는지, 이 기관에 할당되었는지 확인하는 함수 추가

--- ysl/api/__init__.py
+++ ysl/api/__init__.py
@@ -1,8 +1,10 @@
 from flask import request, abort
-from flask_jwt_extended import get_jwt_identity, jwt_required
+from flask_jwt_extended import get_jwt_identity
 from functools import wraps
 
 from ysl.db.agency import Agency
+from ysl.db.belong import Belong
+from ysl.db.access import Access
 from ysl.db import session
 
 
@@ -22,31 +24,25 @@ def check_json(json):
     return decorator
 
 
-def check_admin():
-    def decorator(func):
-        @wraps(func)
-        def wrapeer(*args, **kwargs):
+def check_admin(agency_code):
+    admin = session.query(Agency).filter(
+        Agency.code == agency_code).filter(Agency.email == get_jwt_identity()).first()
 
-            agency = session.query(Agency).query.filter(Agency.email == get_jwt_identity()).first()
-
-            if not agency:
-                abort(403, "No access rights")
-
-            return func(*args, **kwargs)
-        return wrapeer
-    return decorator
+    if not admin:
+        abort(403, "No access rights")
 
 
-def check_agency():
-    def decorator(func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
+def check_agency_belong(agency_code):
+    belong = session.query(Belong).filter(
+        Belong.agency == agency_code).filter(Belong.interviewer == get_jwt_identity()).first()
 
-            agency = session.query(Agency).query.filter(Agency.code == get_jwt_identity()).first()
+    if not belong:
+        abort(403, "No access rights")
 
-            if not agency:
-                abort(400, "unseen agency")
 
-            return func(*args, **kwargs)
-        return wrapper
-    return decorator
+def check_access_interview(interview_id):
+    access = session.query(Access).filter(
+        Access.interview == interview_id).filter(Access.interviewer == get_jwt_identity()).first()
+
+    if not access:
+        abort(403, "No access rights")
