7a2671208b6aeabf6931e04408fdc3ea520a2d45
sineunju <eunjuoi0515@gmail.com>
2019-10-08 17:08:39 +0900

    [ADD] docker image pushed

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -4,12 +4,14 @@ version=`python -c "import ysl; print(ysl.__version__)"`
 
 echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin registry.entrydsm.hs.kr
 
-if [[ "$1" == "api" ]];then
+if [[ "$1" == "develop" ]];then
     echo "Docker build on dev startred"
 
-    docker build -t registry.entrydsm.hs.kr/ysl:dev.
+    docker build -t registry.entrydsm.hs.kr/ysl:develop.
 
-    docker push registry.entrydsm.hs.kr/ysl:dev
+    docker push registry.entrydsm.hs.kr/ysl:develop
+
+    echo "Docker image pushed"
 
 elif [[ "$1" == "master" ]];then
     echo "Docker build on master started"
@@ -20,6 +22,8 @@ elif [[ "$1" == "master" ]];then
 
     docker push registry.entrydsm.hs.kr/ysl:${version}
     docker push registry.entrydsm.hs.kr/ysl:latest
+
+    echo "Docker image pushed"
 
 fi
 
