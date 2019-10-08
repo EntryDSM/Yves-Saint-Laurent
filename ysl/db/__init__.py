adf3355d5fbcf1684b205b76ffbccf5f68d72b9f
sineunju <eunjuoi0515@gmail.com>
2019-10-08 12:21:40 +0900

    [ADD] db url

--- ysl/db/__init__.py
+++ ysl/db/__init__.py
@@ -2,9 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from ysl.config.config import TestConfig
+from ysl.config.config import Config
 
-engine = create_engine(TestConfig.DATABASE_URL)
+engine = create_engine(Config.DATABASE_URL)
 Base = declarative_base()
 
 Session = sessionmaker(bind=engine)
