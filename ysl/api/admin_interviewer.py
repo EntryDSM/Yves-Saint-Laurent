08dec42b2afb906b06a17889af2f6582184ade3e
sineunju <eunjuoi0515@gmail.com>
2019-10-11 01:58:29 +0900

    [ADD] 기관에 소속된 면접관 리스트 api

--- ysl/api/admin_interviewer.py
+++ ysl/api/admin_interviewer.py
@@ -3,6 +3,7 @@ from flask_restful import Resource
 from flask_jwt_extended import jwt_required
 
 from ysl.db.belong import Belong
+from ysl.db.interviewer import Interviewer
 from ysl.db.apply_interviewer import ApplyInterviewer
 from ysl.db import session
 from ysl.api import check_admin, check_json
@@ -53,3 +54,22 @@ class RejectInterviewer(Resource):
             return {"msg": "Successful interviewer reject"}, 200
         else:
             return abort(400, "This is an interviewer who didn't apply")
+
+
+class InterviewerList(Resource):
+    @jwt_required
+    def get(self, agency_code):
+        check_admin(agency_code)
+
+        interviewers = session.query(Belong, Interviewer).join(Interviewer).filter(Belong.agency == agency_code).all()
+
+        if interviewers:
+            return {
+                "interviewer": [
+                    {
+                        "interviewer_name": interviewer.Interviewer.name,
+                        "interviewer_email": interviewer.Interviewer.email
+                    } for interviewer in interviewers]
+            }, 200
+        else:
+            abort('400', "There is no interviewer who is registered.")
